1dceec23bc5402a0f268991b4edab7522ea2e3d4
MattiaManfreda <mattiamanfreda926@gmail.com>
2021-04-07 17:16:23 +0200

    confronto

--- js/script.js
+++ js/script.js
@@ -25,7 +25,15 @@ console.log("numeriGenerati = []", numeriGenerati);
 //parte un setTimeout di 30 secondi (3000 ms) che fa partire una funzione che chiede all'utente con un prompt di inserire 5 numeri e li pusha in un array
 var numeriInseriti = [];
 
+
+
+//crea una scansione dei numeriGenerati.leght con un for () e dentro il for in serire un'altra iterazione per scansionare i numeriInseriti.leght 
+//2 for se numeriGenerati.leght == numeriInseriti.leght allora pusha quel valore in un terzo array e poi incrementa la somma di 1.
+
 setTimeout (inserireNumeri, 2000);
+
+var numeriIndovinati = [];
+var sommaNumeriEsatti = 0;
 
 function inserireNumeri () {
     for (var j = 0; j < 5; j++) {
@@ -34,27 +42,24 @@ function inserireNumeri () {
         numeriInseriti.push(numeroAttuale);
         console.log("Numero Inserito pushato", numeroAttuale);
     }
-}
 
-console.log("numeriInseriti = []", numeriInseriti);
-
-
-//crea una scansione dei numeriGenerati.leght con un for () e dentro il for in serire un'altra iterazione per scansionare i numeriInseriti.leght 
-//2 for se numeriGenerati.leght == numeriInseriti.leght allora pusha quel valore in un terzo array e poi incrementa la somma di 1.
-
-setTimeout (comparaArray, 10000);
-
-function comparaArray (arrayUno, arrayDue) {
-    for (var k = 0; k < arrayUno.length; k++) {
-
+    for (var k = 0; k < numeriGenerati.length; k++) {
+        
         alert("SONO ENTRATO NEL CICLO numeriGenerati");
     
-        for (var z = 0; z < arrayDue.length; z++) {
-            console.log(numeriInseriti);
-            alert("SONO ENTRATO NEL CICLO");
+        for (var z = 0; z < numeriInseriti.length; z++) {
+            console.log("numeriInseriti.length: ", numeriInseriti[z]);
+            
+            if (numeriInseriti[z] == numeriGenerati[k]) {
+                numeriIndovinati.push(numeriInseriti[z]);
+                sommaNumeriEsatti = sommaNumeriEsatti + 1;
+            }
         }
     }
+    console.log(numeriIndovinati);
+    console.log(sommaNumeriEsatti);
 }
+
 
 
 //Infine stampa
